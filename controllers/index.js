8f31e8811419a175d4caa6a8c87ec6bbc71e54c3
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-06-11 19:56:46 -0300

    fixed sending data

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const Task = require("../models/task");
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+    res.status(200).json(tasks);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -16,7 +16,7 @@ const getTask = async (req, res) => {
     if (!task) {
       return res.status(404).json([{ message: "Task not found" }]);
     }
-    res.status(200).json({ task });
+    res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -25,7 +25,7 @@ const getTask = async (req, res) => {
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
-    res.status(201).json({ task });
+    res.status(201).json(task);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
