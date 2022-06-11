05280a3fe09e0b08e95ef0f91e68e6260981827b
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-06-11 13:45:36 -0300

    fixed getting tasks

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const Task = require("../models/task");
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+    res.status(200).json([{ tasks }]);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -14,9 +14,9 @@ const getTask = async (req, res) => {
     const { id: _id } = req.params;
     const task = await Task.findOne({ _id });
     if (!task) {
-      return res.status(404).json({ message: "Task not found" });
+      return res.status(404).json([{ message: "Task not found" }]);
     }
-    res.status(200).json({ task });
+    res.status(200).json([{ task }]);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
