b3438a5583d31ec75f73d712d51b541bf4601ba9
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-06-11 13:50:18 -0300

    installed cors

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const connectDB = require("./db/connect");
 const tasks = require("./routes/index");
 const express = require("express");
 const app = express();
+var cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/tasks", tasks);
