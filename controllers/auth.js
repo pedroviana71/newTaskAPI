8d1f1670f40e5116951bccd699af9dd0f923f987
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-11-28 21:15:08 -0300

    added bcryptjs to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,34 @@
+const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 const register = async (req, res) => {
-  res.send("register");
+  const { password, email, username } = req.body;
+
+  if (!email || !password || !username) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Missing information" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { password: hashedPassword, email, username };
+  const user = await User.create(tempUser);
+
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  const { password, email, username } = req.body;
+
+  if (!email || !password || !username) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: "Missing information" });
+  }
+  res.send("ok");
 };
 
 module.exports = { register, login };
