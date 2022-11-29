d2f4a5e68fdcdd9917746707736c1bdefd6f4ef2
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-11-29 18:12:03 -0300

    added jsonwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,21 +2,22 @@ const { StatusCodes } = require("http-status-codes");
 const User = require("../models/user");
 
 const register = async (req, res) => {
-  const { password, email, username } = req.body;
-
-  if (!email || !password || !username) {
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({ message: "Missing information" });
-  }
-
   try {
-    await User.create({ password, email, username });
-  } catch (error) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
-  }
+    const { password, email, username } = req.body;
 
-  res.status(StatusCodes.CREATED).json({ email, username });
+    if (!email || !password || !username) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Missing information" });
+    }
+
+    const user = await User.create({ password, email, username });
+    const token = user.createJWTToken();
+
+    res.status(StatusCodes.CREATED).json({ email, username, token });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+  }
 };
 
 const login = async (req, res) => {
