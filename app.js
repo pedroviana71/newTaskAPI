0639e334a3a18e8db4e2945f19426f591fee8462
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-12-05 20:55:59 -0300

    updated app js

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 require("dotenv").config()
-const connectDB = require("./db/connect")
 const tasks = require("./routes/tasks")
 const authRouter = require("./routes/auth")
-const express = require("express")
-const app = express()
-var cors = require("cors")
 const auth = require("./middlewares/auth")
 
-app.use(cors())
+const app = express()
+const connectDB = require("./db/connect")
 
+const cors = require("cors")
+const express = require("express")
+const helmet = require("helmet")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
+app.set("trust proxy", 1)
+
+app.use(cors())
+app.use(helmet())
+app.use(xss())
 app.use(express.json())
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+)
+
 app.get("/", (req, res) => {
   res.send("API is running")
 })
