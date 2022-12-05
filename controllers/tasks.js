8695bfb7d5d7c230de5ceb219d52e18b0dca9e63
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-12-05 20:01:05 -0300

    added to find user tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,11 @@
 const Task = require("../models/task")
 
 const getAllTasks = async (req, res) => {
-  const user = req.user
+  const { userId } = req.user
 
   try {
-    const tasks = await Task.find({})
-    res.status(200).json({ tasks, user })
+    const tasks = await Task.find({ _id: userId })
+    res.status(200).json(tasks)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
