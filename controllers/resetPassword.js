e57869b55aab00369d69573062cf10ce269f36fc
pedroviana71 <pedrohenrique.viana5@gmail.com>
2024-04-09 23:26:34 -0300

    installed resend

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const User = require("../models/user");
 const Token = require("../models/tokenPassword");
-const nodemailer = require("nodemailer");
 const { StatusCodes } = require("http-status-codes");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
+const { Resend } = require("resend");
 
 const resetPassword = async (req, res) => {
   try {
     const { email } = req.body;
+    const instanceResend = new Resend("re_QrXv6mVM_LLJZhYeZPsBhcbfPEv9ng15v");
+    console.log(email);
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -30,42 +32,19 @@ const resetPassword = async (req, res) => {
     });
 
     const link = `http://listing.bohr.io/reset-password/${user._id}/${resetToken}`;
-    console.log(user._id, resetToken);
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: "listing.app0@gmail.com",
-        pass: process.env.PASSWORD_MAIL,
-        clientId: process.env.CLIENT_ID_MAIL,
-        clientSecret: process.env.CLIENT_SECRET_MAIL,
-        refreshToken: process.env.REFRESH_TOKEN_MAIL,
-      },
+    await instanceResend.emails.send({
+      from: "pedrohenrique.viana5@gmai..com",
+      to: email,
+      subject: "edefinicao de senha",
+      html: "<strong>it works!</strong>",
     });
 
-    const mailOptions = {
-      from: "listing.app0@gmail.com",
-      to: user.email,
-      subject: "Recuperação de senha - Listing App",
-      text: `Você solicitou um link para recuperação de senha no Listing. Clique no link abaixo para redefinir sua senha. 
-      \n\n
-      ${link}
-      \n\n
-      Se você não solicitou essa recuperação, ignore esse email e sua senha permanecerá inalterada.\n`,
-    };
-
-    transporter.sendMail(mailOptions, function (err, data) {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).send("Email not sent");
-      } else {
-        return res.status(StatusCodes.OK).send("Email sent");
-      }
-    });
+    res
+      .status(StatusCodes.OK)
+      .json({ message: "Password reset link sent to your email" });
   } catch (error) {
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send({ error, message: "Internal server error" });
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ error });
   }
 };
 
